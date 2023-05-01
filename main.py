9fd8c2f6ce1d151714ba06734295f378e067eb2b
Maxim <116120563+it-chep@users.noreply.github.com>
2023-05-01 14:13:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from ai_models import OurModel, HuggingModel
+
+OurModel.get_prediction(img_path='')
+HuggingModel.get_prediction(image_path='')
+
+if __name__ == '__main__':
+    print('helloworld!')
